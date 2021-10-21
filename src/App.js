82ba24d9b7f1ba45b534dce41ccdfe3b2ae89a9b
Koder90 <Koder90>
2021-10-21 21:02:49 +0200

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Content from "./components/Content";
+import "./index.css";
 
 const App = () => {
   const [search, setSearch] = useState("");
-
-
 
   return (
     <div className="App">
       <Header />
       <Input search={search} setSearch={setSearch} />
- <Content />
-    
+      <Content />
     </div>
   );
-}
+};
 
 export default App;
