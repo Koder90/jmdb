57f2f13f01345f8812be4743a727b894d79fe617
Koder90 <Koder90>
2021-11-04 18:17:00 +0100

    added login to page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import '../index.css';
 
 function Header() {
+
+ const [loggedIn, setLoggedIn] = useState(false);
+ console.log(loggedIn)
+
+ const toggleLogin = () => {
+  return setLoggedIn(!loggedIn)
+ }
+
   return (
     <div className="ui inverted huge menu menu">
      <a className="item">
     Home
   </a>
-  <a className="item">
-    My favorites
-  </a>
-  <a className="item">
-    Sign in
-  </a>
+ {loggedIn ? <a className="item">
+    Unavailable
+  </a>: <a className="item">
+    Watched List
+  </a>}
+  {loggedIn ? <a onClick={toggleLogin} className="item">Sign in</a> 
+  : <a onClick={toggleLogin} className="item">Sign out</a>}
   </div>
   )
 }
